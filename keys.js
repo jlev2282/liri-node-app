c6343ad0776d2bc2a3377d9a9cc28e4632f7af92
John <jlev2282@gmail.com>
2017-05-09 21:32:35 -0400

    first commit

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'VNz6dCpl3XvYvFWgSYWHMgRef',
+  consumer_secret: 'tyTEvG7sQOXxseQzmElQQqH7t7YrP39KDoxkb5VTbQF6ygAUPa',
+  access_token_key: '	298805509-8shnqJXs9k4971PlU8dh09rUydSAH5vzeRiLQBz6',
+  access_token_secret: 'P7m7mfbyxRXNORl58tMNqtFvq55GlUiF4wHuGf50R5jPQ',
+}
---
+++ liri.js
@@ -0,0 +1,2 @@
+//Code pulling in info from keys.js
+var keys = require("keys.js");
